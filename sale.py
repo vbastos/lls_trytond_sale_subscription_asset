3df7eafe9fefb0eecc68011c4145abb2b7652bd5
Vincent Bastos <vincentbastos@gmail.com>
2018-08-05 09:16:46 +1000

    add missing import

--- sale.py
+++ sale.py
@@ -1,5 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+import datetime
 from sql import Null
 from sql.conditionals import Coalesce
 
